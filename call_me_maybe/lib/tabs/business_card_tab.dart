fd9ce2ef90bf78979d05ebfd3ea4f099772be09b
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2020-07-12 23:58:33 -0400

    made a stupid image

--- call_me_maybe/lib/tabs/business_card_tab.dart
+++ call_me_maybe/lib/tabs/business_card_tab.dart
@@ -4,7 +4,30 @@ class BusinessCardTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('business car tab'),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            profilePhoto(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget profilePhoto(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        maxWidth: 200.0,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(1000.0),
+        child: Image.asset(
+          'assets/images/profile_photo.PNG',
+          width: MediaQuery.of(context).size.width * 0.5,
+          height: MediaQuery.of(context).size.width * 0.5
+        ),
+      ),
     );
   }
 }
