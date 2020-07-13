ce6b041a185f89a90ebb220d73d5fe5e434133d2
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2020-07-12 22:55:15 -0400

    add image file

--- call_me_maybe/lib/tabs/resume_tab.dart
+++ call_me_maybe/lib/tabs/resume_tab.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class ResumeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Center(
       child: Text('Resume tab'),
     );
