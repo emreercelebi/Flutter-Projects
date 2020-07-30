b843c9d1b9f6244c620f1c7b50c7f7435ced2a94
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2020-07-29 21:00:04 -0400

    journal: boilerplate cleanup

---
+++ journal/lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body: Text(
+          'hello'), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
