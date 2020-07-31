5336a2e564749e23bab944e24f9dd147aa226807
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2020-07-30 23:27:32 -0400

    journal: minimal styling for settings drawer, add dark mode switch

--- journal/lib/settings_drawer.dart
+++ journal/lib/settings_drawer.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
-class SettingsDrawer extends StatelessWidget {
+class SettingsDrawer extends StatefulWidget {
+  @override
+  _SettingsDrawerState createState() => _SettingsDrawerState();
+}
+
+class _SettingsDrawerState extends State<SettingsDrawer> {
+  bool isSwitched = false;
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Text('this is the settings drawer'),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 48.0, 16.0, 16.0),
+            child: Text('Settings'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Dark Mode'),
+            trailing: Switch(
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  this.isSwitched = value;
+                  print(isSwitched);
+                });
+              },
+              activeTrackColor: Colors.lightGreenAccent,
+              activeColor: Colors.green,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
